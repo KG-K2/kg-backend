73a8dfe6a701c8efde95bde4a21729e790394bd4
Adrian <adrian.hamzah15@gmail.com>
2025-12-03 15:07:23 +0700

    feat: add cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from app.models import QueryRequest
 from app.services import run_custom_query
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/run-query")
 async def run_query(request: QueryRequest):
